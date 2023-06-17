c30c447ff21459778e7683abd8ecbb812d357d91
Ambuj Singh <ambujs89@gmail.com>
2023-06-17 19:47:09 +0530

    prototype the chat server

--- apps/backend/src/handler.rs
+++ apps/backend/src/handler.rs
@@ -1,109 +1,115 @@
-use actix_ws::{CloseReason, Message, MessageStream, ProtocolError, Session};
+use actix_ws::{CloseReason, Message as WsMessage, MessageStream, Session};
 use futures_util::StreamExt;
+use std::sync::Arc;
 use std::time::{Duration, Instant};
-use tokio::{pin, select, time::interval};
+use tokio::{select, sync::mpsc, time::interval};
+
+use crate::AppState;
 
 const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
 const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
 
-pub async fn echo_ws(mut session: Session, mut msg_stream: MessageStream) {
+pub async fn chat_ws(mut ctx: Session, mut msg_stream: MessageStream, state: Arc<AppState>) {
     tracing::info!("Connected to Websocket");
 
     let mut last_heartbeat = Instant::now();
     let mut interval = interval(HEARTBEAT_INTERVAL);
 
+    let new_id = fastrand::usize(..9999);
+    let (msg_tx, mut msg_rx) = mpsc::unbounded_channel();
+
+    {
+        let mut sessions = state.sessions.lock().unwrap();
+        sessions.insert(new_id, msg_tx);
+        dbg!(&sessions.len());
+    }
+
+    tracing::info!("User {new_id} connected.");
+
     let reason = loop {
         // create "next client timeout check" future
-        let tick = interval.tick();
-        // required for select()
-        pin!(tick);
+        // let tick = interval.tick();
+        // let ws_messages = msg_stream.next();
 
-        // waits for either `msg_stream` to receive a message from the client or the heartbeat
-        // interval timer to tick, yielding the value of whichever one is ready first
         select! {
-            // received message from WebSocket client
-            Some(message) = msg_stream.next() => {
-                tracing::info!("msg: {message:?}");
+            Some(message) = msg_rx.recv() => {
+                last_heartbeat = Instant::now();
+                ctx.text(message).await.unwrap();
+            }
 
-                if let Err(reason) = handle_message(message, &mut session, &mut last_heartbeat).await {
+            Some(Ok(ws_msg)) = msg_stream.next() => {
+                tracing::debug!("Recieved ws_msg: {ws_msg:?}");
+
+                if let Err(reason) = process_ws_msg(ws_msg, &mut last_heartbeat, &mut ctx, new_id, state.clone()).await {
                     break reason;
                 }
-            },
+            }
 
-            // heartbeat interval ticked
-            _inst = tick => {
-                // if no heartbeat ping/pong received recently, close the connection
+            _ = interval.tick() => {
                 if Instant::now().duration_since(last_heartbeat) > CLIENT_TIMEOUT {
-                    tracing::info!(
-                        "client has not sent heartbeat in over {CLIENT_TIMEOUT:?}; disconnecting"
-                    );
-
+                    tracing::warn!("Client has not sent any heartbeats for more that {CLIENT_TIMEOUT:?}; disconnecting.");
                     break None;
                 }
 
                 // send heartbeat ping
-                let _ = session.ping(b"").await;
+                let _ = ctx.ping(b"").await;
             }
-
-
         }
     };
 
     // attempt to close connection gracefully
-    let _ = session.close(reason).await;
+    let _ = ctx.close(reason).await;
 
-    tracing::info!("disconnected");
+    tracing::info!("User {new_id:?} disconnected");
 }
 
-async fn handle_message(
-    message: Result<Message, ProtocolError>,
-    session: &mut Session,
+async fn process_ws_msg(
+    msg: WsMessage,
     last_heartbeat: &mut Instant,
+    ctx: &mut Session,
+    skip_id: usize,
+    state: Arc<AppState>,
 ) -> Result<(), Option<CloseReason>> {
-    match message {
-        Err(err) => {
-            tracing::error!("{:?}", err);
+    use WsMessage::*;
+
+    match msg {
+        Text(txt) => {
+            for (id, msg_tx) in state.sessions.lock().unwrap().iter() {
+                if *id != skip_id {
+                    msg_tx.send(txt.clone().into()).unwrap();
+                }
+            }
+
+            Ok(())
+        }
+        Binary(_bin) => {
+            tracing::warn!("Did not expect binary data.");
+
             Err(None)
         }
 
-        Ok(msg) => {
-            match msg {
-                Message::Text(text) => {
-                    session.text(text).await.unwrap();
-                    Ok(())
-                }
+        Close(reason) => Err(reason),
 
-                Message::Binary(bin) => {
-                    session.binary(bin).await.unwrap();
-                    Ok(())
-                }
+        Ping(bytes) => {
+            *last_heartbeat = Instant::now();
+            ctx.pong(&bytes).await.unwrap();
 
-                Message::Close(reason) => {
-                    return Err(reason);
-                }
-
-                Message::Ping(bytes) => {
-                    *last_heartbeat = Instant::now();
-                    let _ = session.pong(&bytes).await;
-
-                    Ok(())
-                }
-
-                Message::Pong(_) => {
-                    *last_heartbeat = Instant::now();
-
-                    Ok(())
-                }
-
-                Message::Continuation(_) => {
-                    tracing::warn!("no support for continuation frames");
-
-                    Ok(())
-                }
-
-                // no-op; ignore
-                Message::Nop => Ok(()),
-            }
+            Ok(())
         }
+
+        Pong(_) => {
+            *last_heartbeat = Instant::now();
+
+            Ok(())
+        }
+
+        Continuation(_) => {
+            tracing::warn!("no support for continuation frames");
+
+            Err(None)
+        }
+
+        // ignore
+        _ => Ok(()),
     }
 }
--- apps/backend/src/main.rs
+++ apps/backend/src/main.rs
@@ -1,18 +1,33 @@
+use std::{collections::HashMap, sync::Mutex};
+
 use actix_web::{
-    get, rt,
-    web::{self, ServiceConfig},
+    get,
+    web::{self, Data, ServiceConfig},
     Error, HttpRequest, HttpResponse,
 };
 use shuttle_actix_web::ShuttleActixWeb;
+use tokio::sync::mpsc;
 
-mod chat_server;
 mod handler;
 
-use handler::echo_ws;
+use handler::chat_ws;
+use tokio::task::spawn_local;
+
+pub struct AppState {
+    pub sessions: Mutex<HashMap<usize, mpsc::UnboundedSender<String>>>,
+}
 
 #[shuttle_runtime::main]
 async fn actix_web() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
-    Ok(route_config.into())
+    let app_state = Data::new(AppState {
+        sessions: Mutex::new(HashMap::new()),
+    });
+
+    let config = move |cfg: &mut ServiceConfig| {
+        cfg.configure(route_config).app_data(app_state);
+    };
+
+    Ok(config.into())
 }
 
 fn route_config(cfg: &mut ServiceConfig) {
@@ -25,10 +40,13 @@ async fn hello_world() -> &'static str {
 }
 
 #[get("/ws")]
-async fn ws_handler(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+async fn ws_handler(
+    req: HttpRequest,
+    stream: web::Payload,
+    state: Data<AppState>,
+) -> Result<HttpResponse, Error> {
     let (res, session, msg_stream) = actix_ws::handle(&req, stream)?;
-
-    rt::spawn(echo_ws(session, msg_stream));
+    spawn_local(chat_ws(session, msg_stream, state.into_inner()));
 
     Ok(res)
 }
