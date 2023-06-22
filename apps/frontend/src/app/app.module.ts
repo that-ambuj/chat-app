38e453ba49d917437f34bb945ae7dd358405f998
Ambuj Singh <ambujs89@gmail.com>
2023-06-22 12:00:33 +0530

    remove default nx-welcome component

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 import { CommonUiModule } from '@chat-app/common-ui';
 import { ChatWindowComponent } from './chat-window/chat-window.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, ChatWindowComponent],
+  declarations: [AppComponent, ChatWindowComponent],
   imports: [BrowserModule, CommonUiModule],
   providers: [],
   bootstrap: [AppComponent],
