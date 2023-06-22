c38e35f3ac216e1d5b070f87b78f2d75f69fe26d
Ambuj Singh <ambujs89@gmail.com>
2023-06-22 11:59:17 +0530

    write markup for frontend

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
+import { CommonUiModule } from '@chat-app/common-ui';
+import { ChatWindowComponent } from './chat-window/chat-window.component';
+
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, NxWelcomeComponent, ChatWindowComponent],
+  imports: [BrowserModule, CommonUiModule],
   providers: [],
   bootstrap: [AppComponent],
+  exports: [ChatWindowComponent],
 })
 export class AppModule {}
