0a481bf40e7a123add8fd29fed26159c7f7a18ba
Ambuj Singh <ambujs89@gmail.com>
2023-06-22 11:57:26 +0530

    export all components

---
+++ libs/common-ui/src/index.ts
@@ -0,0 +1,4 @@
+export * from './lib/common-ui.module';
+export * from './lib/title/title.component';
+export * from './lib/button/button.component';
+export * from './lib/chat-input/chat-input.component';
