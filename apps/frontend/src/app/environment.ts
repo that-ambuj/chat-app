2d465b7c079d135746973e52325284e3470e83e1
Ambuj Singh <ambujs89@gmail.com>
2023-06-24 13:26:49 +0530

    fix ws address to be used in dev mode

--- apps/frontend/src/app/environment.ts
+++ apps/frontend/src/app/environment.ts
@@ -1,11 +1,17 @@
 import { isDevMode } from '@angular/core';
 
 const ws_protocol = window.location.protocol == 'https:' ? 'wss://' : 'ws://';
-let ws_host = window.location.hostname;
+let ws_host = window.location.host;
 
-// Use port 8000 when running in local env
-if (isDevMode()) {
-  ws_host += ':8000';
+const port = window.location.port;
+
+// Use port 8000 if the app is not being served from the backend
+if ((port !== '80' && port !== '443' && port !== '8000') || isDevMode()) {
+  const ws_host_arr = ws_host.split(':');
+  ws_host_arr.pop();
+  ws_host_arr.push('8000');
+
+  ws_host = ws_host_arr.join(':');
 }
 
 export const WS_ENDPOINT = ws_protocol + ws_host + '/ws';
