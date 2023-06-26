890583fb1fbb71c15af6bcef84e921fc7f373ae8
Ambuj Singh <ambujs89@gmail.com>
2023-06-26 21:04:01 +0530

    fix environment.ts

--- apps/frontend/src/app/environment.ts
+++ apps/frontend/src/app/environment.ts
@@ -6,12 +6,16 @@ let ws_host = window.location.host;
 const port = window.location.port;
 
 // Use port 8000 if the app is not being served from the backend
-if ((port !== '80' && port !== '443' && port !== '8000') || isDevMode()) {
+if (
+  (!!port && port !== '80' && port !== '443' && port !== '8000') ||
+  isDevMode()
+) {
   const ws_host_arr = ws_host.split(':');
-  ws_host_arr.pop();
-  ws_host_arr.push('8000');
-
-  ws_host = ws_host_arr.join(':');
+  if (ws_host_arr.length > 1) {
+    ws_host_arr.pop();
+    ws_host_arr.push('8000');
+    ws_host = ws_host_arr.join(':');
+  }
 }
 
 export const WS_ENDPOINT = ws_protocol + ws_host + '/ws';
